38cd9dc35d4a54e1cde2507542531f31ad038109
luanamouraa <133766513+luanamouraa@users.noreply.github.com>
2023-05-31 10:58:09 -0300

    Update gráficosalaries.py

--- gráficosalaries.py
+++ gráficosalaries.py
@@ -5,14 +5,3 @@ st.caption('Luana Moura')
 
 df = pd.read_csv('ds_salaries', sep=',')
 st.dataframe(df) 
-
-
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
